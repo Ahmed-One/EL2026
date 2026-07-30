049083a19c5ddf8a992b7ea414774a84d1a76ad0
Ahmed-One <ahmed34guc@gmail.com>
2026-07-30 22:10:38 +0000

    Initiial implementation of logger

--- cpp/session5/lab1_logging_system.cpp
+++ cpp/session5/lab1_logging_system.cpp
@@ -9,8 +9,111 @@
 // Log level enumeration
 enum class Level { DEBUG = 0, INFO = 1, WARN = 2, ERROR = 3, FATAL = 4 };
 
+// Overload the << operator for the Level enum class
+std::ostream& operator<<(std::ostream& os, Level level) {
+    switch (level) {
+        case Level::DEBUG: os << "DEBUG"; break;
+        case Level::INFO:  os << "INFO";  break;
+        case Level::WARN:  os << "WARN";  break;
+        case Level::ERROR: os << "ERROR"; break;
+        case Level::FATAL: os << "FATAL"; break;
+        default:           os << "UNKNOWN"; break;
+    }
+    return os;
+}
+
 // Log entry structure
-class Log {};
+class Log {
+private:
+  Level level;
+  std::string message;
+  std::time_t timestamp;
+public:
+  Log(Level lvl) : level(lvl), timestamp(std::time(nullptr)) { /* ... */  }
+  
+  Level GetLevel() const
+    {
+        return level;
+    }
+
+  const std::string& GetMessage() const
+  {
+      return message;
+  }
+
+  std::time_t GetTimestamp() const
+  {
+      return timestamp;
+  }
+
+  void Append(const std::string& text)
+  {
+      message += text;
+  }
+
+  Log &operator<<(const std::string& text){
+    this->Append(text);
+    return *this;
+  }
+};
+
+namespace LOG
+{
+  std::vector<Log> logs;
+
+  Log& Info()
+  {
+      logs.emplace_back(Level::INFO);
+      return logs.back();
+  }
+
+  Log& Warn()
+    {
+        logs.emplace_back(Level::WARN);
+        return logs.back();
+    }
+  
+  Log& Error()
+  {
+      logs.emplace_back(Level::ERROR);
+      return logs.back();
+  }
+
+  Log& Debug()
+  {
+      logs.emplace_back(Level::DEBUG);
+      return logs.back();
+  }
+
+  Log& Fatal()
+  {
+      logs.emplace_back(Level::FATAL);
+      return logs.back();
+  }
+
+  std::size_t GetLogCount(){
+    return logs.size();
+  }
+
+  bool ContainsMessage(const std::string &msg){
+    for(const Log& log : logs){
+      if(log.GetMessage().find(msg) != std::string::npos){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void Dump(){
+    for(const Log& log : logs){
+      std::time_t t = log.GetTimestamp();
+      std::tm* timeinfo = std::localtime(&t);
+      std::cout<< std::put_time(timeinfo, "%Y-%m-%d %H:%M:%S") << " | " <<
+                  std::setw(5) << log.GetLevel() << " | " <<
+                  log.GetMessage() << std::endl;
+    }
+  }
+}
 
 int main() {
   std::cout << "==============================================\n";
